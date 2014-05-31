46745148bd1953f3d09536ab599e2f8056c4a000
Julien Torrielli <julieneragon@gmail.com>
2014-05-31 22:25:14 +0200

    correctif login dashboard

--- application/server/routing/rest/user.js
+++ application/server/routing/rest/user.js
@@ -1,8 +1,8 @@
 var userProvider = require(global.paths.server + '/database/mysql/tables/user')
-, 	tokenProvider = require(global.paths.server + '/database/mysql/tables/token')
+,   tokenProvider = require(global.paths.server + '/database/mysql/tables/token')
 ,   mysqlTools = require(global.paths.server + '/database/tools/mysql/core')
 ,   moment = require('moment')
-,	user = { get : {}, post : {}, put : {}, delete : {} };
+,   user = { get : {}, post : {}, put : {}, delete : {} };
 
 /********************************[  GET   ]********************************/
 
@@ -29,48 +29,48 @@ user.get.logout = function(request, response) {
 /********************************[  POST  ]********************************/
 
 user.post.authenticate = function(request, response) {
-	var params = request.params
-	,	body = request.body
-	,	witness = true;
+    var params = request.params
+    ,   body = request.body
+    ,   witness = true;
 
-	witness = body.email && body.password ? witness : false;
+    witness = body.email && body.password ? witness : false;
 
-	if(!witness)
-		response.send({'information': 'An error has occurred - missing information', 'email' : body.email, 'password' : body.password });
-	else
-		userProvider.connect(body.email, body.password, function(error, dataUser) {
-			if(dataUser && dataUser[0]) {
-				mysqlTools.generateRandomBytes(32, function(tokenId) {
-					tokenId = encodeURIComponent(tokenId);
-					var token = {
-						id: tokenId,
-						expirationDate: moment().add('days', 1).format('YYYY-MM-DD HH:mm:ss'),
-                        type: 'AUTHENTICATION',
-						origin: request.header("User-Agent"),
-						userId: dataUser[0].id
-					};
-					tokenProvider.create.token(token, function(error, dataToken) {
-						if(!error) {
-							dataUser[0].token = tokenId;
-							delete(dataUser[0].password);
-							delete(dataUser[0].salt);
-							response.send({'user' : dataUser[0]});
-						} else
-							response.send({'information' : 'An error has occurred - ' + error});
-					});
-				});
-			} else {
-				response.send({'information' : 'An error has occurred - bad credentials'});
-			}
-		});
-
+    if(!witness)
+        response.send({'information': 'An error has occurred - missing information', 'email' : body.email, 'password' : body.password });
+    else
+        userProvider.connect(body.email, body.password, function(error, dataUser) {
+            if(dataUser && dataUser[0]) {
+                if(dataUser[0].roleid == 2) {
+                    mysqlTools.generateRandomBytes(32, function(tokenId) {
+                        tokenId = encodeURIComponent(tokenId);
+                        var token = {
+                            id: tokenId,
+                            expirationDate: moment().add('days', 1).format('YYYY-MM-DD HH:mm:ss'),
+                            type: 'AUTHENTICATION',
+                            origin: request.header("User-Agent"),
+                            userId: dataUser[0].id
+                        };
+                        tokenProvider.create.token(token, function(error, dataToken) {
+                            if(!error) {
+                                dataUser[0].token = tokenId;
+                                delete(dataUser[0].password);
+                                delete(dataUser[0].salt);
+                                response.send({'user' : dataUser[0]});
+                            } else
+                                response.send({'information' : 'An error has occurred - ' + error});
+                        });
+                    });
+                } else
+                    response.send({'information' : 'An error has occurred - you are not allowed to log in'});
+            } else
+                response.send({'information' : 'An error has occurred - bad credentials'});
+        });
 
 }
 
 /********************************[  PUT   ]********************************/
 
 /********************************[ DELETE ]********************************/
-
 
 
 module.exports = user;
