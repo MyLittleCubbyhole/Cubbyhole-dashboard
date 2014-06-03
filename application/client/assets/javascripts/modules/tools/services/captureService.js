558b2fddd0db4d5daaf344333daae5974b8add4c
Nicolas Gaignoux <gaignoux.nicolas@gmail.com>
2014-06-03 16:35:22 +0200

    [FEATURE] export

--- application/client/assets/javascripts/modules/tools/services/captureService.js
+++ application/client/assets/javascripts/modules/tools/services/captureService.js
@@ -46,7 +46,7 @@ angular.module('Tools').
 		function generatePDF($node, name, elements) {
 
 			var pdf = new jsPDF();
-				pdf.addHTML($node, function() {
+			pdf.addHTML($node, function() {
 
 				pdf.save(name);
 				for(var i = 0; i<elements.toRemove.length; i++)
@@ -61,11 +61,138 @@ angular.module('Tools').
 
 		return function($node, name) {
 			var elements;
-			name = name || 'download.pdf';
+			name = name || '';
 
 			contextualizeCss(true);
 			elements = convertSVGs($node);
-			generatePDF($node, name, elements);
+			console.log($node.height())
+			html2canvas($node, {
+				onrendered: function(canvas) {
+					console.log(canvas)
+
+					var pdf = new jsPDF('p', 'mm', 'a3');
+					console.log(pdf)
+					//taille pdf px - 794, 1122
+					var image = canvas.toDataURL("image/jpeg", 1.0);
+					contextualizeCss(false);
+
+					var capture = new Image();
+					capture.src = image;
+
+					$('body').append(capture)
+					console.log(capture.height)
+					return true;
+					var MARGIN = 10;
+					var WIDTH = 1122 - MARGIN*4;//794;
+					var HEIGHT = 794 - MARGIN*4;
+					//ratio
+					HEIGHT = (WIDTH/HEIGHT) * WIDTH;
+
+
+					// $('body').append(canvas)
+					var cropCanvas = document.createElement('canvas')
+				
+
+					// console.log(capture.height, capture.height/1122)
+					// console.log('width', capture.width, 794, capture.width/784, '||', capture.height, 1122, capture.height * (784/capture.width))
+
+
+					console.log('width', capture.width, WIDTH, 'ratio: '+(capture.width/WIDTH))
+					console.log('height', capture.height, (capture.height * WIDTH) / capture.width)
+					console.log('increment', (HEIGHT * capture.height) / ( (capture.height * WIDTH) / capture.width ), HEIGHT)
+
+					var maxHeight = capture.height;
+					var projectionMaxHeight = (capture.height * WIDTH) / capture.width;
+					var increment = (HEIGHT * capture.height) / ( (capture.height * WIDTH) / capture.width );
+					var projectionIncrement = HEIGHT;
+					var currentHeight = increment;
+					var part;
+					var y = 0;
+
+
+					var width = document.createAttribute("width");
+					width.value = WIDTH;//794//capture.width;
+					var height = document.createAttribute("height");
+					height.value = currentHeight//HEIGHT;//1122;//capture.height;
+					cropCanvas.setAttributeNode(width);
+					cropCanvas.setAttributeNode(height);
+					var context = cropCanvas.getContext("2d");
+
+					//pillow tag
+					////todo ajouter margin + augmenter taille
+					var logo = new Image();
+					logo.src = '/images/design/logo-big.png';
+					logo.onload = function() {
+						context.fillStyle = "#fff";
+						context.fillRect(0,0,WIDTH,HEIGHT);
+
+						context.drawImage(logo, (WIDTH - 230)/2 , 250, 230, 286);
+						part = cropCanvas.toDataURL("image/jpeg", 1.0);
+						pdf.addImage(part,'JPEG', 0, 0)
+
+						while(maxHeight > 0) {
+							context.clearRect(0, 0, WIDTH, HEIGHT);
+							context.fillStyle = "#fff";
+							context.fillRect(0,0,WIDTH,HEIGHT);
+
+
+							currentHeight = maxHeight - increment < 0 ? maxHeight : currentHeight;
+							console.log(maxHeight - increment, maxHeight, increment, currentHeight)
+
+							context.drawImage(capture, 0, y, capture.width, increment, 0, 0, WIDTH, HEIGHT);
+
+							part = cropCanvas.toDataURL("image/jpeg", 1.0);
+							pdf.addPage();
+							pdf.addImage(part,'JPEG', MARGIN/2, MARGIN/2)
+
+							y += currentHeight;
+							maxHeight -= increment;
+							console.log(maxHeight);
+						}
+						pdf.save('export_'+ name +'.pdf')
+					}
+
+
+
+					//putain de math
+					// var startHeight = HEIGHT * (784/capture.width)
+					// var fullHeight = capture.height * (784/capture.width)//capture.height;
+					// var y = 0;
+					// var h = startHeight;
+					// console.log('=>',fullHeight, h)
+					// while(fullHeight>0) {
+					// 	context.clearRect(0,0, WIDTH,HEIGHT)
+					// 	h = fullHeight - startHeight < 0 ? fullHeight : h;
+
+					// 	console.log(h, fullHeight, startHeight)
+					// 	context.drawImage(capture, 0, y, capture.width, h, 0, 0, 794, HEIGHT);
+					// 	var t = cv.toDataURL("image/jpeg", 1.0);
+					// 	// console.log()
+					// 	pdf.addImage(t,'JPEG', 0, 0)
+					// 	pdf.addPage();
+
+					// 	y += h;
+					// 	fullHeight -= h;
+					// }
+
+
+					//couper au fur et a mesure le canvas principal
+					//load l'image et l'ajouter via addImage
+					// capture.onLoad = function() {
+						// for(var i =0; i<2; i++) {
+
+							// context.drawImage(capture, 0, i*200, capture.width, 200, 0, 0, 794, 300);
+							// var t = cv.toDataURL("image/jpeg", 1.0);
+							// // console.log()
+							// pdf.addImage(t,'JPEG', 0, 0)
+							// pdf.addPage();
+						// }
+						// pdf.save('toto.pdf')
+					// }
+
+					// generatePDF($(canvas), name, elements);
+				}
+			});
 
 		}
 
