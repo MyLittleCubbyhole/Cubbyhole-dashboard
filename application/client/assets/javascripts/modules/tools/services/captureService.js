e36da71648d38036dea7d3624050eefd9a27d621
Nicolas Gaignoux <gaignoux.nicolas@gmail.com>
2014-06-04 22:55:39 +0200

    bugfix

--- application/client/assets/javascripts/modules/tools/services/captureService.js
+++ application/client/assets/javascripts/modules/tools/services/captureService.js
@@ -43,89 +43,77 @@ angular.module('Tools').
 
         }
 
-        function generatePDF($node, name, elements) {
+        function recover(elements) {
 
-            var pdf = new jsPDF();
-            pdf.addHTML($node, function() {
+            for(var i = 0; i<elements.toRemove.length; i++)
+                elements.toRemove[i].child.remove();
 
-                pdf.save(name);
-                for(var i = 0; i<elements.toRemove.length; i++)
-                    elements.toRemove[i].child.remove();
+            for(var i = 0; i<elements.toRecover.length; i++)
+                elements.toRecover[i].parent.appendChild(elements.toRecover[i].child);
 
-                for(var i = 0; i<elements.toRecover.length; i++)
-                    elements.toRecover[i].parent.appendChild(elements.toRecover[i].child);
-
-                contextualizeCss(false);
-            });
+            contextualizeCss(false);
         }
 
-        return function($node, name) {
-            var elements;
-            name = name || '';
+		return function($node, name) {
+			var elements;
+			name = name || '';
 
-            contextualizeCss(true);
-            elements = convertSVGs($node);
-            html2canvas($node, {
-                onrendered: function(canvas) {
-                    console.log(canvas)
+			contextualizeCss(true);
+			elements = convertSVGs($node);
 
-                    var pdf = new jsPDF('p', 'mm', 'a3');
-                    //taille pdf px - 794, 1122
-                    var image = canvas.toDataURL("image/jpeg", 1.0);
-                    contextualizeCss(false);
+			html2canvas($node, {
+				onrendered: function(canvas) {
 
-                    var capture = new Image();
-                    capture.src = image;
+					var pdf = new jsPDF('p', 'mm', 'a3');
 
-                    $('body').append(capture);
+					//taille pdf px - 794, 1122
+					var image = canvas.toDataURL("image/jpeg", 1.0);
 
-                    return true;
-                    var MARGIN = 10;
-                    var WIDTH = 1122 - MARGIN*4;//794;
-                    var HEIGHT = 794 - MARGIN*4;
-                    //ratio
-                    HEIGHT = (WIDTH/HEIGHT) * WIDTH;
+					var capture = new Image();
+					capture.src = image;
 
-                    var cropCanvas = document.createElement('canvas')
+					var MARGIN = 10;
+					var WIDTH = 1122 - MARGIN*4;//794;
+					var HEIGHT = 794 - MARGIN*4;
+					//ratio
+					HEIGHT = (WIDTH/HEIGHT) * WIDTH;
 
-
-                    // console.log(capture.height, capture.height/1122)
-                    // console.log('width', capture.width, 794, capture.width/784, '||', capture.height, 1122, capture.height * (784/capture.width))
-
-                    var maxHeight = capture.height;
-                    var projectionMaxHeight = (capture.height * WIDTH) / capture.width;
-                    var increment = (HEIGHT * capture.height) / ( (capture.height * WIDTH) / capture.width );
-                    var projectionIncrement = HEIGHT;
-                    var currentHeight = increment;
-                    var part;
-                    var y = 0;
+					var cropCanvas = document.createElement('canvas')
+				
+					var maxHeight = capture.height;
+					var projectionMaxHeight = (capture.height * WIDTH) / capture.width;
+					var increment = (HEIGHT * capture.height) / ( (capture.height * WIDTH) / capture.width );
+					var projectionIncrement = HEIGHT;
+					var currentHeight = increment;
+					var part;
+					var y = 0;
 
 
-                    var width = document.createAttribute("width");
-                    width.value = WIDTH;//794//capture.width;
-                    var height = document.createAttribute("height");
-                    height.value = currentHeight//HEIGHT;//1122;//capture.height;
-                    cropCanvas.setAttributeNode(width);
-                    cropCanvas.setAttributeNode(height);
-                    var context = cropCanvas.getContext("2d");
+					var width = document.createAttribute("width");
+					width.value = WIDTH;//794//capture.width;
+					var height = document.createAttribute("height");
+					height.value = currentHeight;//HEIGHT;//1122;//capture.height;
+					cropCanvas.setAttributeNode(width);
+					cropCanvas.setAttributeNode(height);
+					var context = cropCanvas.getContext("2d");
 
-                    //pillow tag
-                    ////todo ajouter margin + augmenter taille
-                    var logo = new Image();
-                    logo.src = '/images/design/logo-big.png';
-                    logo.onload = function() {
-                        context.fillStyle = "#fff";
-                        context.fillRect(0,0,WIDTH,HEIGHT);
+					var canvasHeight = currentHeight;
 
-                        context.drawImage(logo, (WIDTH - 230)/2 , 250, 230, 286);
-                        part = cropCanvas.toDataURL("image/jpeg", 1.0);
-                        pdf.addImage(part,'JPEG', 0, 0)
+					var logo = new Image();
+					logo.src = '/images/design/logo-big.png';
 
-                        while(maxHeight > 0) {
-                            context.clearRect(0, 0, WIDTH, HEIGHT);
-                            context.fillStyle = "#fff";
-                            context.fillRect(0,0,WIDTH,HEIGHT);
+					logo.onload = function() {
+						context.fillStyle = "#fff";
+						context.fillRect(0,0,WIDTH,canvasHeight);
 
+						context.drawImage(logo, (WIDTH - 230)/2 , 250, 230, 286);
+						part = cropCanvas.toDataURL("image/jpeg", 1.0);
+						pdf.addImage(part,'JPEG', 0, 0)
+
+						while(maxHeight > 0) {
+							context.clearRect(0, 0, WIDTH, canvasHeight);
+							context.fillStyle = "#fff";
+							context.fillRect(0,0,WIDTH,canvasHeight);
 
                             currentHeight = maxHeight - increment < 0 ? maxHeight : currentHeight;
 
@@ -138,7 +126,9 @@ angular.module('Tools').
                             y += currentHeight;
                             maxHeight -= increment;
                         }
+
                         pdf.save('export_'+ name +'.pdf')
+                        recover(elements);
                     }
                 }
             });
