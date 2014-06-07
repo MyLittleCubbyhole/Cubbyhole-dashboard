06c794aa977d5bead1782bb549187c43e48618ca
Julien Torrielli <julieneragon@gmail.com>
2014-06-07 17:57:17 +0200

    update config

--- application/client/assets/javascripts/modules/config/constants/configConstants.js
+++ application/client/assets/javascripts/modules/config/constants/configConstants.js
@@ -1,12 +1,12 @@
 angular.module('Config').
     //*/
-    constant('apiUrl', 'http://127.0.0.1:5150/api/').
-    constant('websocketUrl', 'http://127.0.0.1:5150').
+    constant('API_URL', 'http://127.0.0.1:5150/api/').
+    constant('WEBSOCKET_URL', 'http://127.0.0.1:5150').
     /*/
-    constant('apiUrl', 'https://api.trouducube.com/api/').
-    constant('websocketUrl', 'https://api.trouducube.com').
+    constant('API_URL', 'https://api.trouducube.com/api/').
+    constant('WEBSOCKET_URL', 'https://api.trouducube.com').
     /*
-    constant('apiUrl', 'http://www.pillowrc.com/api/').
-    constant('websocketUrl', 'http://www.pillowrc.com:80').
+    constant('API_URL', 'http://www.pillowrc.com/api/').
+    constant('WEBSOCKET_URL', 'http://www.pillowrc.com:80').
     //*/
     constant('socketRoom', '/cubbyhole');
