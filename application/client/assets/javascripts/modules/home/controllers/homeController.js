5de590cfa1fbf935d48d711a963984b4536f7d79
Julien Torrielli <julieneragon@gmail.com>
2014-06-12 12:27:59 +0200

    suppression modale login sur le dashboard

--- application/client/assets/javascripts/modules/home/controllers/homeController.js
+++ application/client/assets/javascripts/modules/home/controllers/homeController.js
@@ -2,21 +2,6 @@ angular.module('Home').
     controller('HomeController', ['$scope', '$location', function($scope, $location) {
         var $local = $scope.Home = {};
 
-        $local.showModalLogin = false;
-
-        $scope.$on('hide', function() {
-            $local.showModalLogin = false;
-        });
-
-        $local.showLoginModal = function() {
-            $scope.Overlay.activated = true;
-            $local.showModalLogin = true;
-        }
-
-        if($location.path() == '/login') {
-            $local.showLoginModal();
-        }
-
         $scope.toString = function() {
             return 'Home';
         }
